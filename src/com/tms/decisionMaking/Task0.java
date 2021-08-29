c678858e3c6e4f6dc0573b5f36496921c85e115f
OlgaK91 <olga.kechina@gmail.com>
2021-08-29 13:00:37 +0300

    changes based on the teacher's comments

--- src/com/tms/decisionMaking/Task0.java
+++ src/com/tms/decisionMaking/Task0.java
@@ -13,41 +13,25 @@ public class Task0 {
         int monthNumber = scanner.nextInt();
         scanner.close();
         switch (monthNumber) {
-            case (1):
+            case 12:
+            case 1:
+            case 2:
                 System.out.println("Winter");
                 break;
-            case (2):
-                System.out.println("Winter");
-                break;
-            case (3):
+            case 3:
+            case 4:
+            case 5:
                 System.out.println("Spring");
                 break;
-            case (4):
-                System.out.println("Spring");
-                break;
-            case (5):
-                System.out.println("Spring");
-                break;
-            case (6):
+            case 6:
+            case 7:
+            case 8:
                 System.out.println("Summer");
                 break;
-            case (7):
-                System.out.println("Summer");
-                break;
-            case (8):
-                System.out.println("Summer");
-                break;
-            case (9):
+            case 9:
+            case 10:
+            case 11:
                 System.out.println("Autumn");
-                break;
-            case (10):
-                System.out.println("Autumn");
-                break;
-            case (11):
-                System.out.println("Autumn");
-                break;
-            case (12):
-                System.out.println("Winter");
                 break;
             default:
                 System.out.println("Invalid month number");
--- src/com/tms/multidimensionalArray/Task3.java
+++ src/com/tms/multidimensionalArray/Task3.java
@@ -6,11 +6,10 @@ package com.tms.multidimensionalArray;
 public class Task3 {
     public static void main(String[] args) {
         int[][] array = {{1, 1}, {1, 2}, {1, 3}};
-        int sum =0;
-        for (int [] a: array
-             ) {
-            for (int i : a
-                 ) {sum +=i;
+        int sum = 0;
+        for (int[] a : array) {
+            for (int i : a) {
+                sum += i;
             }
         }
         System.out.println(sum);
